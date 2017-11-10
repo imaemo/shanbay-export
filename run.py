ec022ca17c6f5918839e7115c9fafc065f98f319
maemo <tcpln2009@gmail.com>
2017-11-10 17:46:21 +0800

    basic function ok

--- run.py
+++ run.py
@@ -3,8 +3,11 @@
 
 
 import requests
+import os
+from jinja2 import Environment, FileSystemLoader
 
 today = 'https://www.shanbay.com/api/v1/bdc/stats/today'
+word_base_url = 'https://www.shanbay.com/api/v1/bdc/library/today/?page='
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75'
@@ -13,6 +16,7 @@ headers = {
 
 
 def main():
+    result = {}
     cj_dict = {}
     plain_cookie = open('cookies.txt').read()
     for p in plain_cookie.split(';'):
@@ -24,9 +28,47 @@ def main():
     r = s.get(today, headers=headers)
     if r.status_code != 200:
         print 'error, status code:', r.status_code
+    __data = r.json()['data']
+    print __data
+    result['num_passed'] = __data['num_passed']
+    result['num_total'] = __data['num_total']
+    result['used_minutes'] = __data['used_minutes']
     with open('review.json', 'wb') as fd:
         for chunk in r.iter_content(1024):
             fd.write(chunk)
+
+    template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'template')
+    j2_env = Environment(loader=FileSystemLoader(template_dir), trim_blocks=True)
+
+    total_page = 1
+    word_list = []
+    word_url = word_base_url + str(total_page)
+
+    r = s.get(word_url, headers=headers)
+    if r.status_code != 200:
+        print 'status code:', r.status_code
+    else:
+        __data = r.json()
+        for w in __data['data']['objects']:
+            word = {'content': w['content'], 'pron': w['pron'], 'definition': w['definition'], 'audio': w['audio']}
+            word_list.append(word)
+        total_page = __data['data']['total'] / __data['data']['ipp']
+        for curr_page in range(2, total_page + 1):
+            if curr_page == 5:
+                break
+            word_url = word_base_url + str(curr_page)
+            r = s.get(word_url, headers=headers)
+            if r.status_code != 200:
+                print 'status code:', r.status_code
+            else:
+                __data = r.json()
+                for w in __data['data']['objects']:
+                    word = {'content': w['content'], 'pron': w['pron'], 'definition': w['definition'],
+                            'audio': w['audio']}
+                    word_list.append(word)
+    result['words'] = word_list
+
+    j2_env.get_template('wordList.html').stream(result).dump('wordList.html')
 
 
 if __name__ == '__main__':
